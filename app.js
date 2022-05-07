2be935bdd7f9ffbc1e34db7746cde82336f6eaf3
Joe Marcus <josephcmarcus@gmail.com>
2022-05-07 00:09:23 -0400

    Create basic math functions

---
+++ app.js
@@ -0,0 +1,20 @@
+function add(numOne, numTwo) {
+    return numOne + numTwo;
+};
+
+function subtract(numOne, numTwo) {
+    return numOne - numTwo;
+};
+
+function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+};
+
+function divide(numOne, numTwo) {
+    return numOne / numTwo;
+};
+
+console.log(add(5, 5));
+console.log(subtract(3, 2));
+console.log(multiply(5, 5));
+console.log(divide(10, 2));
